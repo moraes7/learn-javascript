d4ccbb5eb49f2ef965b455158f584957dff137a4
Nicolas Moraes <nicolasmoraes_7@outlook.com>
2024-06-05 15:38:47 -0300

    conditional if, else, else if, ? :

---
+++ learn-fundamentals/conditional-branching/else.js
@@ -0,0 +1,7 @@
+// A instrução if pode conter um bloco else opcional. Ele é executado quando a condição é falsa.
+let year = prompt("Em que ano estamos?")
+if(year == 2025) {
+    alert("Você está correto")
+} else {
+    alert("Errado!")
+}
