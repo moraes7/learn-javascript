76cea33909cf4119aec8578aa5d4873b4a4e89bf
Nicolas Moraes <nicolasmoraes_7@outlook.com>
2024-06-05 12:16:39 -0300

    comparisons

---
+++ learn-fundamentals/comparisons/string-comparison.js
@@ -0,0 +1,4 @@
+// Para ver se uma string é maior que outra, o JavaScript usa a chamada ordem “dicionário” ou “lexicográfica”. Em outras palavras, as strings são comparadas letra por letra.
+alert("Z" > "A"); // true
+alert("Glow" > "Glee"); // true
+alert("Bee" > "Be"); // true
