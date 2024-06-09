1a75a425d73a53eb06d15bf28b530d77218c85d5
Nicolas Moraes <nicolasmoraes_7@outlook.com>
2024-06-08 21:43:48 -0300

    atualizando condicionais

--- learn-fundamentals/conditional-branching/else-if.js
+++
@@ -1,9 +0,0 @@
-// else if passa para a próxima condição se for false
-let year = prompt("Em que ano estamos?")
-if(year < 2024) {
-    alert("Estamos a frente")
-} else if (year > 2024) {
-    alert("Estamos atrás")
-} else {
-    alert("Exato!")
-}
--- learn-fundamentals/conditional-branching/if.js
+++
@@ -1,5 +0,0 @@
-// A instrução if avalia uma condição entre parênteses e, se o resultado for true, executa um bloco de código.
-let year = prompt("Em que ano estamos?")
-if(year == 2024) {
-    alert("Correto!")
-}
