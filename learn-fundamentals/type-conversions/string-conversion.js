5e43e28d57404778f657069d9de5517734e66832
Nicolas Moraes <nicolasmoraes_7@outlook.com>
2024-06-09 19:21:11 -0300

    atualizando switch

--- learn-fundamentals/type-conversions/string-conversion.js
+++
@@ -1,8 +0,0 @@
-// O string conversion acontece quando precisamos converter um valor para uma string
-let value = true;
-alert(typeof value); // boolean
-
-value = String(value); // agora o value é uma string "true"
-alert(typeof value); // string
-
-// A conversão de string é principalmente óbvia. O false torna-se "false", null torna-se "null", etc.
--- learn-fundamentals/variables/const.js
+++
@@ -1,9 +0,0 @@
-// variáveis constantes (imutáveis) não podem ser retribuídas, uma tentativa disso causaria um erro
-const nome = "Nicolas";
-nome = "Fulano" // mensagem de erro
-
-// constantes MAIÚSCULAS 
-const COLOR_RED = "#F00";
-const COLOR_GREEN = "#0F0"; // COLOR_GREEN é muito mais fácil de lembrar do que o hexadecimal
-
-let color = COLOR_GREEN;
