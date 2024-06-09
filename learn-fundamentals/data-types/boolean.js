eefe5318c00f55930c325bec5e3cdda50d746de5
Nicolas Moraes <nicolasmoraes_7@outlook.com>
2024-06-08 23:21:12 -0300

    atualizando functions

--- learn-fundamentals/data-types/boolean.js
+++
@@ -1,9 +0,0 @@
-// Boolean tem apenas dois valores: true e false
-// Este tipo é comumente usado para armazenar valores sim/não: true significa “sim, correto” e false significa “não, incorreto”.
-
-let nomeChecado = true; // sim, nome checado
-let idadeChecado = false; // não, idade não checado
-
-// Os valores booleanos também vêm como resultado de comparações:
-let maiorQue = 4 > 1; 
-console.log(maiorQue); // "true"
