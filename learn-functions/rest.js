776f381f9fc72176ffe477817b582b632780f058
Nicolas Moraes <nicolasmoraes_7@outlook.com>
2024-06-17 10:09:33 -0300

    functions

---
+++ learn-functions/rest.js
@@ -0,0 +1,6 @@
+function clube(nome, ...titulos) {
+    log(`Nome do clube: ${nome}`)
+    log(`Títulos: ${titulos}`)
+}
+
+clube("Sociedade Esportiva Palmeiras", "12 Brasileiros", " 3 Libertadores", " 26 Paulistas", " 4 Copas do Brasil")
